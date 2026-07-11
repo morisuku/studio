a3df112caec66b4a736071de00b428a505159883
morisuku <morisuku2009@yahoo.co.jp>
2026-07-11 15:50:18 +0900

    Add files via upload

--- components.jsx
+++ components.jsx
@@ -58,7 +58,7 @@ function Hero({ showSparkles }) {
       <div className="hero-inner">
         <div className="hero-copy">
           <div className="hero-eyebrow">
-            <span>morisuku studio · 2026.7 OPEN<br />香川県高松市一宮町151-1</span>
+            <span>morisuku studio · 2026.8 OPEN<br />香川県高松市一宮町151-1</span>
           </div>
           <h1>
             ときめきを<br />
